be654f60076b918400aedaf14745a12024fe9fff
vjumpkung <vjumpkung@gmail.com>
2023-12-11 04:22:18 +0700

    initial commit

---
+++ src/pages/[url].tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Link from "next/link";
+
+const post = ({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-900">
+      <div>
+        {data == null ? (
+          <div>
+            <h1 className="text-5xl font-mono py-5">Invalid link</h1>
+            <Link href="/">
+              <center>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded disabled:bg-blue-300 disabled:cursor-not-allowed">
+                  Go back home
+                </button>
+              </center>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            <h1>Redirecting.......</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}url/${context.query.url}/`
+    );
+    const data = await res.json();
+    return {
+      props: { url: data.url },
+      redirect: {
+        destination: data !== "null" ? data.url : "",
+        permanent: false,
+      },
+    };
+  } catch {
+    return { props: { url: null } };
+  }
+};
+
+export default post;
